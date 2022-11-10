48ed6e8450b851ed654bf24990d0c8f0f3ab0a2c
coxmd <coxmusyoki@gmail.com>
2022-11-10 21:20:18 +0300

    Fixed responsiveness on mobile phones

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Set current year
+
+const year = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear;
+
+// Mobile navigation
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+btnNav.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
